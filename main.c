5407ed52f7e49465a9768ea6ec0072cf77fde678
Ibrux11 <ibraluunge@gmaiL.com>
2024-04-18 11:05:47 +0200

    main continues

--- main.c
+++ main.c
@@ -4,8 +4,15 @@
 #define PROMPT "@cisfun$"
 
 int main(void)
+    char *buffer;
+    int buffer_size = 1024;
+    buffer = ((char *)malloc(buffer_size * size_of(char))); 
     char*input;
-    size_t bufsize=1024;
     int status;
 
-    
+while(1)
+{
+    printf(PROMPT);
+    input=
+}
+free(buffer);
