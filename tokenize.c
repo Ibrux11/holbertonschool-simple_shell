fd643f6e161843f18aac8a6c462d49e9d68248c7
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-04-26 12:21:51 +0200

    added bypass.c, getenv.c, getline.c, tokenizer.c, getpath.c, values.c, and modified execute.c, shell.c, shell.h, and removed main.c and read_command.c

--- tokenize.c
+++ tokenize.c
@@ -1,42 +1,38 @@
 #include "shell.h"
-
 /**
- * tokenize -  splits a string into smaller parts.
- * @uinput: Commands received from the user.
- * @stoken: store the tokens.
- * Return: return the number of tokens.
-*/
-char **tokenize(char *uinput, int *count)
+ * tokenize - tokenizes a stirng
+ * @lineptr: what the user inputed
+ * Return: a ptr to arr of ptrs
+ */
+
+char **tokenize(char *lineptr)
 {
-char *delim = " \n";
-char *token;
-char **stoken = NULL;
-char *buffercpy = strdup(uinput);
+	char **user_command = NULL;
+	char *token = NULL;
+	size_t i = 0;
+	int size = 0;
 
-if (buffercpy == NULL)
-{
-return (NULL);
-}
+	if (lineptr == NULL)
+		return (NULL);
 
-stoken = malloc(MAX_ARGS * sizeof(char *));
+	for (i = 0; lineptr[i]; i++)
+	{
+		if (lineptr[i] == ' ')
+			size++;
+	}
+	if ((size + 1) == _strlen(lineptr))
+		return (NULL);
+	user_command = malloc(sizeof(char *) * (size + 2));
+	if (user_command == NULL)
+		return (NULL);
 
-if (stoken == NULL)
-{
-free(buffercpy);
-return (NULL);
-}
+	token = strtok(lineptr, " \n\t\r");
 
-token = strtok(buffercpy, delim);
-
-*count = 0;
-while (token != NULL)
-{
-stoken[*count] = strdup(token);
-(*count)++;
-token = strtok(NULL, delim);
-}
-
-stoken[*count] = NULL;
-free(buffercpy);
-return (stoken);
+	for (i = 0; token != NULL; i++)
+	{
+		user_command[i] = token;
+		token = strtok(NULL, " \n\t\r");
+	}
+	user_command[i] = NULL;
+	return (user_command);
 }
